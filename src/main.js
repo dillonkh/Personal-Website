44847e5bb0d9c13b363de7f01fce2eedb8e47bec
Dillon Harris <dillon.keith.harris96@gmail.com>
2021-01-15 09:00:24 -0700

    Last fix for weird image issues

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from "vue";
 import router from "./router";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import browserDetect from "vue-browser-detect-plugin";
-Vue.use(browserDetect);
+
 Vue.config.productionTip = false;
 
 new Vue({
